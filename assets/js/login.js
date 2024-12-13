bb778bdf2a39f1811b330b67673e197715d37012
Creeford Sanchez <creewrk@gmail.com>
2024-12-13 09:10:30 -0800

    started-validation

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,22 @@
 'use strict';
 
 import { listen, select, selectAll, style } from "./data/utility.js";
+
+const loginBtn = select('.login')
+const email = select('.email');
+const password = select('.password');
+let valid = true;
+listen(loginBtn, 'click', () => {
+  if (valid) {
+    reset();
+    window.location.href = ('./index.html');
+  }
+});
+
+function validation() {
+  
+}
+function reset() {
+  email.value = '';
+  password.value = '';
+}
