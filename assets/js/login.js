9f1649efed41f27ba9fbb2bb5ed02eea7d53651d
Creeford Sanchez <creewrk@gmail.com>
2024-12-13 10:42:04 -0800

    creade-comparing-credentials

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,25 +6,24 @@ const loginBtn = select('.login')
 const email = select('.email');
 const password = select('.password');
 const emailError = select('.email-error');
-const passError = select('.password-error')
-let valid = true;
+const passError = select('.password-error');
 
+sessionStorage.setItem('email','cree@gmail.com');
+sessionStorage.setItem('pass', '12345');
 
 listen(loginBtn, 'click', () => {
-  console.log('click');
   if (validation()) {
     reset();
-    //window.location.href = ('./index.html');
+    window.location.href = ('./index.html');
   }
 });
 
 function validation() {
   let emailVal = email.value;
   let passVal = password.value;
-  
 
   if (sufficiantData(emailVal, passVal)) {
-    return true;
+    return compareCredentials(emailVal, passVal);
   }
 
   return false;
@@ -35,18 +34,32 @@ function sufficiantData(email, pass) {
   if (email === '') {
     emailError.innerText = 'Enter Email';
     isValid = false;
+  } else {
+    emailError.innerText = '';
   }
 
   if (pass === '') {
     passError.innerText = 'Enter Password';
     isValid = false;
+  } else {
+    passError.innerText = '';
   }
 
   return isValid;
 }
-function isEmailValid(value) {
-  
+
+function compareCredentials(email, pass) {
+  let emailCredential = sessionStorage.getItem('email');
+  let passCredential = sessionStorage.getItem('pass');
+
+  if (emailCredential !== email || passCredential !== pass) {
+      emailError.innerText = 'Incorrect Email or Password';
+      return false;
+  }
+
+  return true;
 }
+
 function reset() {
   email.value = '';
   password.value = '';
