061effc20a8414963c3aa2ff3ebce0d393655f74
Creeford Sanchez <creewrk@gmail.com>
2024-12-13 09:39:12 -0800

    added-print-error-function

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,15 +5,46 @@ import { listen, select, selectAll, style } from "./data/utility.js";
 const loginBtn = select('.login')
 const email = select('.email');
 const password = select('.password');
+const emailError = select('.email-error');
+const passError = select('.password-error')
 let valid = true;
+
+
 listen(loginBtn, 'click', () => {
-  if (valid) {
+  console.log('click');
+  if (validation()) {
     reset();
-    window.location.href = ('./index.html');
+    //window.location.href = ('./index.html');
   }
 });
 
 function validation() {
+  let emailVal = email.value;
+  let passVal = password.value;
+  
+
+  if (sufficiantData(emailVal, passVal)) {
+    return true;
+  }
+
+  return false;
+}
+
+function sufficiantData(email, pass) {
+  let isValid = true;
+  if (email === '') {
+    emailError.innerText = 'Enter Email';
+    isValid = false;
+  }
+
+  if (pass === '') {
+    passError.innerText = 'Enter Password';
+    isValid = false;
+  }
+
+  return isValid;
+}
+function isEmailValid(value) {
   
 }
 function reset() {
